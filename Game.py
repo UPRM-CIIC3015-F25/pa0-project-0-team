eab9d036ff761f6318fcaf8641729a3a063f90be
gear21-ctrl <gabriel.andujar1@upr.edu>
2025-09-06 13:32:35 -0400

    TASK 5 DONE, MERGE CONFLICT SOLVED

--- Game.py
+++ Game.py
@@ -11,7 +11,7 @@ def ball_movement():
     ball.y += ball_speed_y
 
     # Start the ball movement when the game begins
-    # TODO Task 5 Create a Merge Conflict
+    # DONE Task 5 Create a Merge Conflict
     speed = 10
     if start:
         ball_speed_x = speed * random.choice((1, -1))  # Randomize initial horizontal direction
